84f191ae0bce244fc1e17a4df3d39ef4806f06b7
Demitri <demitrilucienclark@gmail.com>
2024-09-20 10:35:45 -0400

    ehhh

--- Chess.py
+++ Chess.py
@@ -7,7 +7,7 @@ Board = [["r","h","b","q","k","b","h","r"],
          ["p","p","p","p","p","p","p","p"],
          ["_","_","_","_","_","_","_","_"],
          ["_","_","_","_","_","_","_","_"],
-         ["_","_","_","_","_","_","_","_"],
+         ["P","P","_","r","_","_","P","P"],
          ["_","_","_","_","_","_","_","_"],
          ["P","P","P","P","P","P","P","P"],
          ["R","H","B","Q","K","B","H","R"]]
@@ -24,12 +24,19 @@ def move():
     new1 = int(input("Enter Y: "))
 
     print(Board[pos1][pos2])
+    print("next")
 
     if Board[pos1][pos2]=="h" or Board[pos1][pos2]=="H":
         move_knight(pos1,pos2,new1,new2)
 
-    if Board[pos1][pos2]=="p" or Board[pos1][pos2]=="P":
+    elif Board[pos1][pos2]=="p" or Board[pos1][pos2]=="P":
         move_pawn(pos1,pos2,new1,new2)
+
+    elif Board[pos1][pos2]=="k" or Board[pos1][pos2]=="K":
+        move_king(pos1,pos2,new1,new2)
+    
+    elif Board[pos1][pos2]=="r" or Board[pos1][pos2]=="R":
+        move_rook(pos1,pos2,new1,new2)
 
 
     print_board()
@@ -73,7 +80,7 @@ def move_pawn(P1,P2,N1,N2):
     if Board[P1][P2]=="p":   # check for lowercase
         if(abs(N2-P2)==1 and N1-P1==1): # If move is diagonal attack
             if(not(Board[N1][N2]=="_")):     # checking for piece on diagonal
-                if(not is_friendly_piece(P1,P2,N1,N2)):
+                if(not is_friendly_piece(P1,P2, N1,N2)):
                     Board[N1][N2] = Board[P1][P2]
                     Board[P1][P2] = "_"
                 else:
@@ -87,7 +94,29 @@ def move_pawn(P1,P2,N1,N2):
                 print("cant move pawn to occupied spot")
         else:
             print("Pawns cannot make this move")
-    #else:                   # else uppercase move
+   
+   
+    # else uppercase move
+   
+   
+    else:  
+        if(abs(P2-N2)==1 and P1-N1==1): # If move is diagonal attack
+            if(not(Board[N1][N2]=="_")):     # checking for piece on diagonal
+                if(not is_friendly_piece(P1,P2, N1,N2)):
+                    Board[N1][N2] = Board[P1][P2]
+                    Board[P1][P2] = "_"
+                else:
+                    print("friendly piece")
+            print("no piece on diagonal")
+        elif(N2==P2 and P1-N1==1):                          # If move is forward move
+            if(Board[N1][N2]=="_"):
+                Board[N1][N2] = Board[P1][P2]
+                Board[P1][P2] = "_"
+            else:
+                print("cant move pawn to occupied spot")
+        else:
+            print("Pawns cannot make this move")
+
     move()
 
 
@@ -113,6 +142,52 @@ def move_knight(P1,P2,N1,N2):
     move()
     
        
+#================================================================================#
+
+def move_king(P1,P2,N1,N2):
+
+    # ADD A CHECK WHICH COMBINES ALL OTHER PIECES MOVING ABILITIES BACKWARDS TO SEE IF KING IS STILL IN 'CHECK'
+
+    if((abs(N1-P1)==1) or (abs(N2-P2)==1) or ((abs(N2-P2)==1)and(abs(N1-P1)==1))):
+        if(is_inbound(N1,N2) and not is_friendly_piece(P1,P2,N1,N2)):
+            Board[N1][N2] = Board[P1][P2]
+            Board[P1][P2] = "_"
+        else:
+            print("not inbound or trying to move onto friendly piece ")
+    else:
+        print("invalid king movement")
+
+    move()
+
+
+#================================================================================#
+
+def move_rook(P1,P2,N1,N2):
+    canmove= True
+
+    if(P1==N1): # HORIZONAL!! => This means there is no vertical change
+        if(N2>P2)and(N2-P2>1): # MOVING RIGHT!! ------->  moving more than 1 spot!
+            for index in range(P2+1,N2):
+                print(Board[P1][index])
+                if not (Board[P1][index] == "_"): # If the current space inbetween the current pos and new pos isnt a "_" 
+                    canmove = False
+        
+        # MOVING RIGHT WORKS AS INTENTED, MOVING LEFT RIGHT NOW LETS USER SKIP PIECES
+
+        #elif(N2<P2)and(P2-N2>1): # MOVING LEFT!! <-------  moving more than 1 spot!
+        #    for index in range(P2+1,N2):
+        #        if not (Board[P1][index] == "_"): # If the current space inbetween the current pos and new pos isnt a "_" 
+        #            canmove = False
+    
+
+    if(canmove):
+        Board[N1][N2] = Board[P1][P2]
+        Board[P1][P2] = "_"
+
+    (move)
+
+
+
 #================================================================================#
      
 move()
